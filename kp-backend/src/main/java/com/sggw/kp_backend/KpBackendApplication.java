6fb8ca87b99dcb6f596c5e60623406858c72b55a
Gwozdz1uuu <gwozdeckidaniel@gmail.com>
2026-04-11 15:01:31 +0200

    initial spring projects added

---
+++ kp-backend/src/main/java/com/sggw/kp_backend/KpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sggw.kp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpBackendApplication.class, args);
+	}
+
+}
