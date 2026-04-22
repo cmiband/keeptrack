991ca955f64aafe7629157aaf84147174b5ee731
xKsOm <adam.domzalowicz@gmail.com>
2026-04-22 19:19:48 +0200

    origin/869cyz3y7
    home page

--- kp-frontend/src/app/home/home.ts
+++ kp-frontend/src/app/home/home.ts
@@ -9,9 +9,33 @@ import { AuthService } from '../auth.service';
   styleUrl: './home.css',
 })
 export class Home {
+
+  activebutton: string = 'list';
+
+  boards: [string, number, string][] = [
+    ['projectszpont', 7, '#4CAF50'],
+    ['fdp-palindromy', 3, '#FF9800'],
+    ['splendor', 22, '#2196F3'],
+    ['sprawdko-swiecik', 4, '#8BC34A'],
+    ['keeptrack2', 4, '#E91E63']
+  ];
+
+  users: [string, string, string][] = [
+    ['Barosz', 'Adamowicz', '#D37EF1'],
+    ['Witold', 'Filipek', '#99E98F'],
+    ['Igor', 'Kucharski', '#E99B8F'],
+    ['Igor', 'Czarnogłowski', '#83E5F4'],
+    ['Albert', 'Gmitrzak', '#F48385'],
+    ['Daniel', 'Gwozdecki', '#F4ED83']
+  ];
+
+  currentUser = ['Adam', 'Domzalowicz'];
+
+  directMeesageExpand: boolean = true;
+
   ngOnInit() {
     const token = localStorage.getItem('token');
-    
+
     console.log('--- SESSION TEST ---');
     if (token) {
       console.log('Token found:', token);
@@ -19,12 +43,20 @@ export class Home {
       console.warn('No token in localStorage!');
     }
   }
-  
+
   private authService = inject(AuthService);
   private router = inject(Router);
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  handleClick(name: string) {
+    this.activebutton = name;
+  }
+
+  toggleDirectMessages() {
+    this.directMeesageExpand = !this.directMeesageExpand;
   }
 }
